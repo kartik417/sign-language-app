4c5905e636efeb6a2f001b3aecea4f3eb1d319f5
Kartik Sharma <kartiksharma242001@gmail.com>
2026-04-24 14:12:35 +0530

    add .gitignore file to exclude node_modules and env files

--- backend/server.js
+++ backend/server.js
@@ -24,4 +24,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("MONGO URI STATUS:", process.env.MONGO_URI ? "FOUND" : "NOT FOUND");
 });
